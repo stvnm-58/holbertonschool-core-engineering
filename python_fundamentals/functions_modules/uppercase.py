1f3709e43aa3dfd8e74f8d07c96509e8ee0af39c
steven <12992@holbertonstudents.com>
2026-05-06 12:50:55 +0200

    uppercase

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
+
     for i in str:
         if 97 <= ord(i) <= 122:
-            char_majuscule = chr(ord(i) - 32)
-            print(char_majuscule, end="")
+            print(chr(ord(i) - 32), end="")
+            
         else:
             print(i, end="")
     
+    print("")
