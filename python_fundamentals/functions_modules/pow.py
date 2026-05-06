d65a6251bc900c597e9c682930890ddfa0b95b77
steven <12992@holbertonstudents.com>
2026-05-06 13:12:48 +0200

    pow

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-def pow(a, b):
+ def pow(a, b):
+
     result = 1
 
-    if b == 0:
-        return result
-
     for i in range(b):
-        result = result * a
-        return result
+         result = result * a
+
+    return result 
