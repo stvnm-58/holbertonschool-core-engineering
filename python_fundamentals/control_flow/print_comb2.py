e776e2522eea0a682bc8998fa5462f4e6ffa14ce
steven <12992@holbertonstudents.com>
2026-05-06 11:02:41 +0200

    correction

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-for a in range(0 , 9):
-    for b in range(0 , 9):
-        print("{}{}".format(a, b), end="")
-        if a != 9 and b != 9:
-            print(", ", end="")
+for number in range(0, 100):
+    if number < 99:
+        print("{}".format(number), end=", ")
+
+    else:
+        print("{}".format(number))
