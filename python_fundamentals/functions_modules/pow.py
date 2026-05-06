2c32284455af87961e320d8442366e6535adb8d2
steven <12992@holbertonstudents.com>
2026-05-06 13:15:01 +0200

    pow

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,9 +2,16 @@
 
 def pow(a, b):
 
+    if b == 0:
+        return 1
+
+    exp = b if b > 0 else -b
+
     result = 1
+    for i in range(exp):
+        result = result * a
+      
+    if b < 0:
+        return 1 / result
 
-    for i in range(b):
-         result = result * a
-
-    return result 
+    return result
