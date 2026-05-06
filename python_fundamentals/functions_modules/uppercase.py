cb46c2466a3841365b89c5618153d291e246075c
steven <12992@holbertonstudents.com>
2026-05-06 12:57:02 +0200

    uppercase

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -3,10 +3,10 @@
 def uppercase(str):
     for char in str:
         ascii_val = ord(char)
-        
+
         if 97 <= ascii_val <= 122:
             char = chr(ascii_val - 32)
-        
+
         print("{}".format(char), end="")
-    
+
     print("")
