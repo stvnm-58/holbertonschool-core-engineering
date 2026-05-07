092c2264613b18280ea5085a43f6b3e1e1569244
steven <12992@holbertonstudents.com>
2026-05-07 12:38:25 +0200

    calculation

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from calculation_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("{}".format(add(a, b)))
+    print("{}".format(sub(a, b)))
+    print("{}".format(mul(a, b)))
+    print("{}".format(div(a, b)))
