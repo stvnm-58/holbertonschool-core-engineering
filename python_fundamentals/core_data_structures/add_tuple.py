70ee7b6f32d2c6de512977c17a7b42b649c2a6b1
steven <12992@holbertonstudents.com>
2026-05-08 12:07:01 +0200

    tuple

--- python_fundamentals/core_data_structures/add_tuple.py
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[0]
     else:
         a1 = 0
-    
+
     if len(tuple_a) >= 2:
         a2 = tuple_a[1]
     else:
@@ -16,7 +16,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1 = tuple_b[0]
     else:
         b1 = 0
-        
+
     if len(tuple_b) >= 2:
         b2 = tuple_b[1]
     else:
