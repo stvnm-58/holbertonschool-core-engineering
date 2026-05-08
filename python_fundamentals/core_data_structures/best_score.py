30d46d90f9b8f595e04a4c82e585f121560e4fb1
steven <12992@holbertonstudents.com>
2026-05-08 14:35:23 +0200

    best score

--- python_fundamentals/core_data_structures/best_score.py
+++ python_fundamentals/core_data_structures/best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
-    return max(a_dictionary.get, key=a_dictionary.get)
+    best = max(a_dictionary, key=a_dictionary.get)
+    return best
