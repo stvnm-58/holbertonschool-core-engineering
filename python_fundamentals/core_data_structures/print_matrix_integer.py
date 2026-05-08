ef652cc5dafea22a6857d5b6ea604e6c1f6c7cf1
steven <12992@holbertonstudents.com>
2026-05-08 11:35:56 +0200

    Print a matrix

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range (len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+
+        print("")
