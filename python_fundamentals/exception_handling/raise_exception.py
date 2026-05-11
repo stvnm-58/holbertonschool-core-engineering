54d5bc94b430c2b3629edf995e2b616578510105
steven <12992@holbertonstudents.com>
2026-05-11 12:09:50 +0200

    Exception Handling

---
+++ python_fundamentals/exception_handling/raise_exception.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def raise_exception():
+    raise TypeError()
---
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def safe_print_list(my_list=[], x=0):
+    nb_print = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            nb_print = nb_print + 1
+        except IndexError:
+
+            break
+    
+    print("")
+    return nb_print
+    
