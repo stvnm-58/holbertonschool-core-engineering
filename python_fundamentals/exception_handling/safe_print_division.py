fb36562c4538dccdcaae2f14d20d2753aa22b441
steven <12992@holbertonstudents.com>
2026-05-13 11:09:06 +0200

    correction

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
         result = a / b
     except (ZeroDivisionError, TypeError):
         result = None
-        
     finally :
         print("Inside result: {}".format(result))
+
     return result
