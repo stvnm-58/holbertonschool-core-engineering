8f6f7ac5a48a0edd54ef59a253767d29624919d4
steven <12992@holbertonstudents.com>
2026-05-13 11:05:17 +0200

    correction

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -8,8 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_print = nb_print + 1
         except (ValueError, TypeError):
             pass
-        except IndexError:
-            break
 
     print("")
     return nb_print
