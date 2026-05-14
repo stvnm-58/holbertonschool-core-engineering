6da57e5e3de6849b0202b25968d390c57b06adaa
steven <12992@holbertonstudents.com>
2026-05-14 17:44:19 +0200

    square

---
+++ python_oop/inheritance/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module définissant la classe Square."""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """Représente un carré qui hérite de BaseGeometry."""
+
+    def __init__(self, size):
+        """
+        Initialise un nouveau carré.
+
+        Args:
+            size (int): La taille du côté du carré.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calcule la surface du carré.
+
+        Returns:
+            int: Le résultat de size multiplié par lui-même.
+        """
+        return self.__size * self.__size
