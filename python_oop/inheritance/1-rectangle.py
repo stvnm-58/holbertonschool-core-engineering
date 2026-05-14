84ce30e33b4a92fa4f7a9d2ec8f7c520f43aff31
steven <12992@holbertonstudents.com>
2026-05-14 13:19:18 +0200

    rectagle

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Rectangle:
+    def __init__(self, width, height):
+        
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        
+        self.__width = width
+        self.__height = height
