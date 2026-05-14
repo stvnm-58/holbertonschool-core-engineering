187d989ec67f0610dd6112168a2bb1e31e0367e7
steven <12992@holbertonstudents.com>
2026-05-14 14:22:16 +0200

    rectangle

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
+"""Module définissant la classe Rectangle, héritant de BaseGeometry."""
 
-class Rectangle:
+
+class Rectangle(BaseGeometry):
+    """Représente un rectangle utilisant BaseGeometry."""
+
     def __init__(self, width, height):
-        
+        """
+        Initialise un nouveau Rectangle.
+
+        Args:
+            width (int): La largeur du rectangle.
+            height (int): La hauteur du rectangle.
+        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
+        """
+        Calcule l'aire du rectangle.
+
+        Returns:
+            int: La surface du rectangle.
+        """
         return self.__width * self.__height
-    
+
     def __str__(self):
-        return f"rectangle {self.__width}/{self.__height}"
+        """
+        Retourne la représentation sous forme de chaîne du rectangle.
+
+        Returns:
+            str: Description du rectangle au format [Rectangle] width/height.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
