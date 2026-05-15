de1ffedb7c18c60c1f1f53835109991b89a5efdf
steven <12992@holbertonstudents.com>
2026-05-15 17:28:01 +0200

    tramme verboselist

--- python_oop/abc/verboselist.py
+++ python_oop/abc/verboselist.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+class VerboseList(list)
+
+
+def append avec super()
+
+def pop avec l'index -1
+
+def extend sur même principe que append
+
+def remove même principe que plus haut
