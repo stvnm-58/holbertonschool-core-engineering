be4c9136468f559933e84cc0b5b585f4a0ca490c
steven <12992@holbertonstudents.com>
2026-07-07 11:21:45 +0200

    correction

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -3,13 +3,18 @@
 import asyncio
 from websockets.asyncio.server import serve
 
-async def echo(websocket):
+
+async def connection_handler(websocket):
+    """Handle the websocket connection and echo back messages."""
     async for message in websocket:
         await websocket.send(message)
 
+
 async def main():
-    async with serve(echo, "localhost", 8765) as server:
+    """Start the websocket server."""
+    async with serve(connection_handler, "localhost", 8765) as server:
         await server.serve_forever()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
